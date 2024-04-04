70c1721a51241b5b404a25d4af59762946551395
Luc <phamtienluc0601@gmail.com>
2024-04-04 17:45:37 +0700

    update the overview part

---
+++ assets/js/CTDT.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() 
+{
+    const midPic = document.getElementById('app_overview-picContainer-midPic');
+    const mainPic = document.getElementById('app_overview-picCardMain');
+
+    
+});
